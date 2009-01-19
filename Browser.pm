f8e46bb353bc93f7a5cb4c9d828dc5b828cda1f0
Ben Crowell <githubcrowell09@lightandmatter.com>
2009-01-18 21:28:51 -0800

    fix crash when opening OG 2.x format and giving wrong password

--- Browser.pm
+++ Browser.pm
@@ -75,7 +75,7 @@ sub main_loop {
     $browser_data->{GB}->undo_callback(sub{$browser_window->adjust_after_undo(@_)});
   }
   $browser_window->set_footer_text(w("startup_info"));
-  if (defined $browser_data->{GB}->when_last_modified()) {
+  if (ref $browser_data->{GB} && defined $browser_data->{GB}->when_last_modified()) {
     $browser_window->is_modified(1);
   }
   $mw->repeat(5000,\&periodic_actions); # every 5000 milliseconds, i.e., once every 5 seconds
