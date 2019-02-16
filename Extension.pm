d99b76486b27a86c21067aa72c128b2b5139c0dc
Ben Crowell <githubcrowell09@lightandmatter.com>
2019-02-15 16:00:03 -0800

    change syntax for extension code, require language prepended

--- Extension.pm
+++ Extension.pm
@@ -5,21 +5,32 @@ use IPC::Run;
 
 
 sub apply_scalar_function_of_x {
-  my $f = shift; # source code, with x as the input
-                 # example using lisp: "* x 0.1"
-                 # example using bc: "x*0.1"
+  my $code = shift; # source code, with x as the input
+                 # example using lisp: "guile:* x 0.1"
+                 # example using bc: "bc:x*0.1"
   my $x = shift;
                  # As a convenience feature, if $f is undefined or null string, we return the input unaltered,
                  # and likewise if $x$ is a null string.
-  if (!defined $f || $f eq '' || $x eq '') {return $x}
-  if (1) { # bc
+  if (!defined $code || $code eq '' || $x eq '') {return $x}
+  unless ($code=~/\A([a-z]+):(.*)\Z/s) {
+    extension_err("Error executing code, no language specified:\ncode:\n$code\n");
+    return undef;
+  }
+  my ($language,$f) = ($1,$2);
+  if ($language eq 'bc') {
     my $source_code = "define f(x) {\nreturn($f)}\nf($x)\n";
     return execute_bc_code($source_code);
   }
-  if (0) { # Guile
+  if ($language eq 'guile') {
+    if (1) {
+      extension_err("Error executing code, guile disabled for security reasons:\ncode:\n$f\n");
+      return undef;
+    }
     my $source_code = "(display ((lambda (x) ($f)) $x))";
     return execute_guile_code($source_code);
   }
+  extension_err("Error executing code, unrecognized language: $language\ncode:\n$code\n");
+  return undef;
 }
 
 sub execute_bc_code {
@@ -31,7 +42,7 @@ sub execute_bc_code {
     return $out;
   }
   else {
-    print STDERR "Error executing code using bc:\n$err\ncode:\n$source_code\n";
+    extension_err("Error executing code using bc:\n$err\ncode:\n$source_code\n");
     return undef;
   }
 }
@@ -42,10 +53,16 @@ sub execute_guile_code {
   my $source_code = shift; # the source code of a complete Guile program
   my $result;
   if (not eval { $result = IPC::System::Simple::capturex('guile','-c',$source_code); 1 }) {
-      print STDERR "Error executing scheme code using guile:\n$@.\n";
+      extension_err("Error executing scheme code using guile:\n$@.\n");
       return undef;
   }
   return $result;
 }
+
+sub extension_err {
+  my $message = shift;
+  print STDERR $message;
+}
+
 
 1;
